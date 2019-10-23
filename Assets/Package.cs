46c739dccb19c12ce8e3e953d88f0b2385dc1f27
wilsonchooo <wcho1205@bths.edu>
2019-10-23 17:55:24 -0400

    box+throw+enemy

---
+++ Assets/Package.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Package : MonoBehaviour
+{
+    public static bool retrieved = true;
+    public SpriteRenderer sprite;
+    public Collider2D boxcollid;
+    public Rigidbody2D rigid;
+    public int boxspeedx;
+    public int boxspeedy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        boxcollid = GetComponent<Collider2D>();
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (retrieved == true)
+        {
+            transform.position = GameObject.FindGameObjectWithTag("Player").transform.position;
+            sprite.sortingLayerName = "Player";
+        }
+
+        else if(retrieved ==false)
+        {
+            //transform.position = transform.position;
+            sprite.sortingLayerName = "Package";
+        }
+
+
+
+        if (boxcollid.IsTouching(GameObject.FindGameObjectWithTag("Enemy").GetComponent<Collider2D>()))
+        {
+            Debug.Log("yeet");
+            rigid.AddForce(new Vector3(transform.position.x - boxspeedx, transform.position.y + boxspeedy));
+        }
+
+    }
+
+
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
     [System.Serializable]
@@ -12,18 +13,46 @@ public class Player : MonoBehaviour
     }
     public PlayerStats stats = new PlayerStats();
     public int fallheight = -20;
+    public int dashspeed = 20;
+    public float dist;
+    public float throwspeed = 10;
+    public int throwx;
+    public int throwy;
+    public Collider2D playercollider;
+    public Rigidbody2D rigi;
 
+
+    private void Start()
+    {
+         Rigidbody2D rigi = GameObject.FindGameObjectWithTag("Package").GetComponent<Rigidbody2D>();
+        playercollider = GetComponent<Collider2D>();
+}
     private void Update()
     {
         if (transform.position.y <= fallheight)
         {
             DamagePlayer(2);
         }
-
+        if (Package.retrieved==false)
+            UnityStandardAssets._2D.PlatformerCharacter2D.m_MaxSpeed = 20f;
+        
+        else 
+            UnityStandardAssets._2D.PlatformerCharacter2D.m_MaxSpeed = 10f;
+        
+        if(playercollider.IsTouching(GameObject.FindGameObjectWithTag("Enemy").GetComponent<Collider2D>()))
+        {
+            Debug.Log("yeet");
+            touchenemy();
+        }
+        
         dash();
+        dropbox();
+        throwbox();
+
+
     }
 
-    public void DamagePlayer (int dmg)
+    public void DamagePlayer(int dmg)
     {
         stats.Health -= dmg;
         if (stats.Health <= 0)
@@ -37,20 +66,72 @@ public class Player : MonoBehaviour
     public void dash()
     {
         if (Input.GetKeyDown("c"))
-       {
+        {
             Debug.Log("clicked");
-            if (gameObject.GetComponent<UnityStandardAssets._2D.PlatformerCharacter2D>().m_FacingRight== true)
+            if (gameObject.GetComponent<UnityStandardAssets._2D.PlatformerCharacter2D>().m_FacingRight == true)
             {
                 transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+                //UnityStandardAssets._2D.PlatformerCharacter2D.m_MaxSpeed = 20f;
+
             }
 
             if (gameObject.GetComponent<UnityStandardAssets._2D.PlatformerCharacter2D>().m_FacingRight != true)
             {
                 transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+                //UnityStandardAssets._2D.PlatformerCharacter2D.m_MaxSpeed = 20f;
             }
 
 
         }
     }
-    
+
+    public void dropbox()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+
+
+            dist = Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Package").transform.position);
+            if (dist < 3)
+                Package.retrieved = !Package.retrieved;
+
+
+
+
+
+        }
+    }
+
+    public void throwbox()
+    {
+        float step = throwspeed * Time.deltaTime;
+        if (Input.GetKeyDown("t"))
+        {
+            if (Package.retrieved == true)
+            {
+                //GameObject.FindGameObjectWithTag("Package").transform.position = Vector3.MoveTowards(GameObject.FindGameObjectWithTag("Package").transform.position, new Vector3(transform.position.x+10, transform.position.y), step);
+
+                //rigi.AddForce(new Vector3(GameObject.FindGameObjectWithTag("Package").transform.position.x + throwx, GameObject.FindGameObjectWithTag("Package").transform.position.y +throwy));
+                if (gameObject.GetComponent<UnityStandardAssets._2D.PlatformerCharacter2D>().m_FacingRight)
+                    rigi.AddForce(new Vector3(transform.position.x + throwx, transform.position.y + throwy));
+                if (!gameObject.GetComponent<UnityStandardAssets._2D.PlatformerCharacter2D>().m_FacingRight)
+                    rigi.AddForce(new Vector3(transform.position.x - throwx, transform.position.y + throwy));
+
+
+                Package.retrieved = false;
+            }
+        }
+
+    }
+
+    public void touchenemy()
+    {
+        if (Package.retrieved)
+        Package.retrieved = false;
+
+        else if (!Package.retrieved)
+        {
+
+        }
+    }
 }
